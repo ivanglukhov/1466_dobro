74bb3098f1b5540d8c0b7c9e53ee6ec6d4845f91
ivanglukhov <90202979+ivanglukhov@users.noreply.github.com>
2024-11-28 15:17:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 from flask import Flask
+from flask import render_template
+import database
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("index.html")
+
+@app.route("/advs")
+def advs():
+    rows = database.get_all_adverts()
+    advs = []
+    for adv in rows:
+        d = {}
+        d['id'] = adv[0]
+        d['adress'] = adv[1]
+        d['header'] = adv[2]
+        d['description'] = adv[3]
+        d['service_mode'] = adv[4]
+        d['creator'] = adv[5]
+        d['image'] = adv[6]
+
+        advs.append(d)
+    return render_template("board.html", advs=advs)
+
+@app.route("/advs/<id>")
+def adv(id):
+    adv = database.get_advert(id)
+    d = {}
+    d['id'] = adv[0]
+    d['adress'] = adv[1]
+    d['header'] = adv[2]
+    d['description'] = adv[3]
+    d['service_mode'] = adv[4]
+    d['creator'] = adv[5]
+    d['image'] = adv[6]
+
+    adv = d
+    return render_template('adv.html', adv=adv)
 
 app.run()
